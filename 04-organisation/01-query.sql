5d4c2537a39ca6ec9040d8e8801dc4e7088e7e7c
Sergei Kamianetskii <i@ksergey.ru>
2024-06-23 13:25:27 +0300

    Создание таблицы employees

---
+++ 04-organisation/01-query.sql
@@ -0,0 +1,21 @@
+CREATE TABLE employees (
+    id INTEGER
+    name TEXT
+    position TEXT
+    salary REAL
+
+CREATE TABLE employees (
+    id INTEGER
+    name TEXT
+    position TEXT
+    salary REAL
+);
+
+CREATE TABLE employees (
+    id INTEGER,
+    name TEXT,
+    position TEXT,
+    salary REAL
+);
+
+DROP TABLE employees;
